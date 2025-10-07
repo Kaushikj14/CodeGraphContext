4ec793a7a98d043feb9ee629aef4ae7783801cf4
kaushik Jain <83067256+Kaushikj14@users.noreply.github.com>
2025-10-07 11:38:32 +0530

    Create insert.py

---
+++ insert.py
@@ -0,0 +1,10 @@
+class Solution:
+    #Function to insert a node in a BST. 
+    def insert(self,root, Key):
+        if root is None:
+            return Node(Key)
+        if root.data > Key:
+            root.left = self.insert(root.left,Key)
+        elif root.data<Key:
+            root.right = self.insert(root.right,Key)
+        return root
